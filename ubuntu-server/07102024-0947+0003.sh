db5b6eb49c50f163f81bf46cb24838fd79596424
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-07 09:55:15 +0300

    Ubuntu Server 24.04 LTS configuration from HP 15-bw0xx
    Capture date and time: October 7, 2024, 9:47 AM, UTC+03

---
+++ ubuntu-server/07102024-0947+0003.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Snapshot date and time: October 7, 2024, 9:47 AM, UTC+03
+# Snapshot taken on: HP 15-bw0xx
+
+# Specs of the computer the snapshot was taken on:
+# AMD A4-9120 with Radeon R3 Graphics / Core layout: 2C2G
+# 16 GB 1067 MHz DDR4 SODIMM RAM
+# 480 GB KIOXIA EXCERIA-SSD
+# Secure boot: Enabled
+# TPM revision: 2.0
+
+# Ubuntu Pro - Uncomment only if you are on an LTS release and have an eligible Ubuntu One account.
+#sudo pro attach
+
+# Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+# Uncomment below if you want to bring Tailscale up as soon as it's installed above.
+#sudo tailscale up
