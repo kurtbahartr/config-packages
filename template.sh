3babe0b1593874a952950f458e4c9e27afc6f4db
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-08 15:35:03 +0300

    Add a reference script to make things easier

---
+++ template.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Snapshot date and time: January 1, 1970, 12:00 AM, UTC+00
+# Snapshot taken on: Reference brand Reference model
+
+# Specs of the computer the snapshot was taken on:
+# Reference CPU / Core layout: 1C1G
+# 128 MB 100 MHz DDR RAM
+# 4 GB Reference HDD
+# Secure boot: N/A
+# TPM revision: N/A
+
+# This is a reference script.
+true
+
