da6ee27a85bdc3de44b108306cf1577b818a3d88
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-11 18:52:37 +0300

    Fix the Discord APT repo entry

--- common/discord-apt-repo.sh
+++ common/discord-apt-repo.sh
@@ -4,7 +4,7 @@
 
 # Discord
 sudo wget -O /etc/apt/trusted.gpg.d/discord-apt.gpg.asc https://palfrey.github.io/discord-apt/discord-apt.gpg.asc
-echo "https://palfrey.github.io/discord-apt/debian/ ./" | sudo tee /etc/apt/sources.list.d/discord.list > /dev/null
+echo "deb https://palfrey.github.io/discord-apt/debian/ ./" | sudo tee /etc/apt/sources.list.d/discord.list > /dev/null
 sudo apt update
 sudo apt -y install discord
 
