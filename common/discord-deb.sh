d6824d65b5bf97b0b431b20f6c66bccfaa7f2d63
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-11 22:20:08 +0300

    Update to the new APT sources format

--- common/discord-deb.sh
+++ common/discord-deb.sh
@@ -3,8 +3,8 @@
 # Common script to install Discord from an unofficially maintained APT repo.
 
 # Discord
-sudo wget -O /etc/apt/trusted.gpg.d/discord-apt.gpg.asc https://palfrey.github.io/discord-apt/discord-apt.gpg.asc
-echo "deb https://palfrey.github.io/discord-apt/debian/ ./" | sudo tee /etc/apt/sources.list.d/discord.list > /dev/null
+sudo wget -O /etc/apt/sources.list.d/discord.sources https://github.com/kurtbahartr/config-rootfs/raw/master/common-deb/etc/apt/sources.list.d/discord.sources
+(echo -n "Signed-By:" && curl -sL https://palfrey.github.io/discord-apt/discord-apt.gpg.asc | sed 's/^/ /') | sudo tee -a /etc/apt/sources.list.d/discord.sources > /dev/null
 sudo apt update
 sudo apt -y install discord
 
--- common/spotify-deb.sh
+++ common/spotify-deb.sh
@@ -3,7 +3,8 @@
 # Common script to install and configure Spotify.
 
 # Spotify
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo wget -O /etc/apt/sources.list.d/spotify.sources https://github.com/kurtbahartr/config-rootfs/raw/master/common-deb/etc/apt/sources.list.d/spotify.sources
+# TODO: Find a way to determine the latest key on Spotify downloads.
+(echo -n "Signed-By:" && curl -sL https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sed 's/^/ /') | sudo tee -a /etc/apt/sources.list.d/spotify.sources > /dev/null
 sudo apt update
 sudo apt -y install spotify-client
