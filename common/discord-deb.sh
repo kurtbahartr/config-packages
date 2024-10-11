f832d9b170d304638dae9c9a29d41d63f96eea96
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-11 22:51:19 +0300

    Simplify the way new APT sources files are gathered

--- common/discord-deb.sh
+++ common/discord-deb.sh
@@ -3,7 +3,7 @@
 # Common script to install Discord from an unofficially maintained APT repo.
 
 # Discord
-sudo wget -O /etc/apt/sources.list.d/discord.sources https://github.com/kurtbahartr/config-rootfs/raw/master/common-deb/etc/apt/sources.list.d/discord.sources
+sudo wget -O {,https://github.com/kurtbahartr/config-rootfs/raw/master/common-deb}/etc/apt/sources.list.d/discord.sources
 (echo -n "Signed-By:" && curl -sL https://palfrey.github.io/discord-apt/discord-apt.gpg.asc | sed 's/^/ /') | sudo tee -a /etc/apt/sources.list.d/discord.sources > /dev/null
 sudo apt update
 sudo apt -y install discord
--- common/spotify-deb.sh
+++ common/spotify-deb.sh
@@ -3,7 +3,7 @@
 # Common script to install and configure Spotify.
 
 # Spotify
-sudo wget -O /etc/apt/sources.list.d/spotify.sources https://github.com/kurtbahartr/config-rootfs/raw/master/common-deb/etc/apt/sources.list.d/spotify.sources
+sudo wget -O {,https://github.com/kurtbahartr/config-rootfs/raw/master/common-deb}/etc/apt/sources.list.d/spotify.sources
 # TODO: Find a way to determine the latest key on Spotify downloads.
 (echo -n "Signed-By:" && curl -sL https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sed 's/^/ /') | sudo tee -a /etc/apt/sources.list.d/spotify.sources > /dev/null
 sudo apt update
