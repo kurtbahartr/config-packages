dd4902f71829ffaf50c312b9409621dac8a6702c
Bahar KURT <kurtbahartr@users.noreply.github.com>
2024-12-21 02:44:43 -0500

    Improve the way PPAs are added

--- common/telegram-ppa-deb.sh
+++ common/telegram-ppa-deb.sh
@@ -5,9 +5,7 @@
 # Telegram Desktop
 DEBIAN_VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | sed 's/.*=//')
 UBUNTU_DEBIAN_BASE_CODENAME=$(cat /etc/debian_version | sed 's/\/.*//')
-wget https://github.com/kurtbahartr/config-packages/raw/master/lib/dc2uc.sh
-UBUNTU_LTS_CODENAME=$(source dc2uc.sh $DEBIAN_VERSION_CODENAME 2>&1 || source dc2uc.sh $UBUNTU_DEBIAN_BASE_CODENAME)
-rm -f dc2uc.sh
+UBUNTU_LTS_CODENAME=$(source <(curl -s https://raw.githubusercontent.com/kurtbahartr/config-packages/master/lib/dc2uc.sh) $DEBIAN_VERSION_CODENAME 2>&1 || source <(curl -s https://raw.githubusercontent.com/kurtbahartr/config-packages/master/lib/dc2uc.sh) $UBUNTU_DEBIAN_BASE_CODENAME)
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/ppa-telegram.gpg adv --keyserver keyserver.ubuntu.com --recv-key 25402D509B042DD9
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/ppa-telegram.gpg adv --keyserver keyserver.ubuntu.com --recv-key 6A9653F936FD5529
 sudo add-apt-repository -y -S "deb https://ppa.launchpadcontent.net/atareao/telegram/ubuntu $UBUNTU_LTS_CODENAME main"
--- common/wihotspot-ppa-deb.sh
+++ common/wihotspot-ppa-deb.sh
@@ -4,7 +4,11 @@
 
 # Wihotspot
 DEBIAN_VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | sed 's/.*=//')
-sudo add-apt-repository -y ppa:lakinduakash/lwh || \
-    sudo apt-key --keyring /etc/apt/trusted.gpg.d/ppa-linux-wifi-hotspot.gpg adv --keyserver keyserver.ubuntu.com --recv-key 87B8838C5E2893D3 && \
-    sudo add-apt-repository -y -S "deb https://ppa.launchpadcontent.net/lakinduakash/lwh/ubuntu $(curl -fsSL https://github.com/kurtbahartr/config-packages/raw/master/lib/dc2uc.sh | source /dev/stdin $DEBIAN_VERSION_CODENAME) main"
+UBUNTU_DEBIAN_BASE_CODENAME=$(cat /etc/debian_version | sed 's/\/.*//')
+UBUNTU_LTS_CODENAME=$(source <(curl -s https://raw.githubusercontent.com/kurtbahartr/config-packages/master/lib/dc2uc.sh) $DEBIAN_VERSION_CODENAME 2>&1 || source <(curl -s https://raw.githubusercontent.com/kurtbahartr/config-packages/master/lib/dc2uc.sh) $UBUNTU_DEBIAN_BASE_CODENAME)
+sudo add-apt-repository -y ppa:lakinduakash/lwh
+if [ "$?" != "0" ]; then
+  sudo apt-key --keyring /etc/apt/trusted.gpg.d/ppa-linux-wifi-hotspot.gpg adv --keyserver keyserver.ubuntu.com --recv-key 87B8838C5E2893D3 && \
+  sudo add-apt-repository -y -S "deb https://ppa.launchpadcontent.net/lakinduakash/lwh/ubuntu $UBUNTU_LTS_CODENAME main"
+fi
 sudo apt -y install linux-wifi-hotspot haveged
