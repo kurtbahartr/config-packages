752afeda78852053e2a2cb013f4c9c4cdbfb11fb
Bahar KURT <kurtbahartr@users.noreply.github.com>
2024-12-28 08:41:21 -0500

    Telegram-PPA-DEB: Use GPG instead of apt-key

--- common/telegram-ppa-deb.sh
+++ common/telegram-ppa-deb.sh
@@ -6,7 +6,7 @@
 DEBIAN_VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | sed 's/.*=//')
 UBUNTU_DEBIAN_BASE_CODENAME=$(cat /etc/debian_version | sed 's/\/.*//')
 UBUNTU_LTS_CODENAME=$(source <(curl -s https://raw.githubusercontent.com/kurtbahartr/config-packages/master/lib/dc2uc.sh) $DEBIAN_VERSION_CODENAME 2>&1 || source <(curl -s https://raw.githubusercontent.com/kurtbahartr/config-packages/master/lib/dc2uc.sh) $UBUNTU_DEBIAN_BASE_CODENAME)
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/ppa-telegram.gpg adv --keyserver keyserver.ubuntu.com --recv-key 25402D509B042DD9
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/ppa-telegram.gpg adv --keyserver keyserver.ubuntu.com --recv-key 6A9653F936FD5529
+sudo gpg --keyring /etc/apt/trusted.gpg.d/ppa-telegram.gpg adv --keyserver keyserver.ubuntu.com --recv-key 25402D509B042DD9
+sudo gpg --keyring /etc/apt/trusted.gpg.d/ppa-telegram.gpg adv --keyserver keyserver.ubuntu.com --recv-key 6A9653F936FD5529
 sudo add-apt-repository -y -S "deb https://ppa.launchpadcontent.net/atareao/telegram/ubuntu $UBUNTU_LTS_CODENAME main"
 sudo apt install -y telegram
