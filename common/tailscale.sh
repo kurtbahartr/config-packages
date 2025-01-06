14b55e08251e14eeab21ff57ec74afe9a3c0cdfe
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-01-06 17:27:46 +0300

    Tailscale: Change os-release codename to Oracular if Devel

--- common/tailscale.sh
+++ common/tailscale.sh
@@ -3,7 +3,11 @@
 # Common script to install and configure Tailscale.
 
 # Tailscale
+# Comment out below if you don't want to cause jank with other processes somehow
+sudo cp /etc/os-release{,.bak}
+sudo sed -i '/UBUNTU_CODENAME/ s/devel/oracular/' /etc/os-release
 curl -fsSL https://tailscale.com/install.sh | sh
+sudo mv /etc/os-release{.bak,}
 # Comment out below if you don't want to bring Tailscale up as soon as it's installed above.
 sudo tailscale up
 
