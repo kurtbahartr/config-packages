c793fcd6de9b35e36834962d01236aaf8d0ac5d5
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-01-08 13:35:31 +0300

    lunar-ubuntu: Switch to upstream SysScripts repo for rolling release

--- common/lunar-ubuntu.sh
+++ common/lunar-ubuntu.sh
@@ -12,12 +12,11 @@
 # Client use-case patch is maintained by Linda St-Denis.
 
 # Lunar-Ubuntu
-curl --remote-name -L https://shell.lunarlabs.cc/src/distros/ubuntu.sh
-curl --remote-name -L https://shell.lunarlabs.cc/asset/sysctl.conf
+curl --remote-name -L https://raw.githubusercontent.com/ohemilyy/SysScripts/main/LunarShell/src/distros/ubuntu.sh
 patch -p1 < <(https://raw.githubusercontent.com/kurtbahartr/config-packages/master/patches/lunar-ubuntu.patch)
-sudo cp /etc/sysctl.conf{,.bak}
 sudo bash ubuntu.sh
 rm ubuntu.sh
-rm sysctl.conf
-echo -e "\nLunar Shell also configures /etc/sysctl.conf for network security and several QoL improvements."
-echo "If you don't like the current configuration and think it's because of this config change, I created a backup for you in the same directory with a .bak extension."
+chsh -s /usr/bin/zsh
+echo "Your shell has been changed to ZSH and it has been configured to provide an experience close to Bash."
+echo -e "If you don't like this, you can always switch back with the following command:\n"
+echo -e "  chsh -s /bin/bash\n"
