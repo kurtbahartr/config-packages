2ef40fb0980043a26847f0b9d7110b95f770ddc2
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-02-02 13:48:08 +0300

    discord-deb: Hack into Discord shortcut for appindicator if Unity
    The version of Electron Discord uses fails to acknowledge Unity7,
    so let's force it to think we're on KDE instead.

--- common/discord-deb.sh
+++ common/discord-deb.sh
@@ -8,3 +8,7 @@ sudo apt install -y ./discord-repo_1.0_all.deb
 rm discord-repo_1.0_all.deb
 sudo apt update
 sudo apt -y install discord-canary
+if echo $XDG_CURRENT_DESKTOP | grep -i unity > /dev/null; then
+  sudo sed -i '/Exec=/ s/Exec=/Exec=env XDG_CURRENT_DESKTOP=kde /' /usr/share/applications/discord-canary.desktop
+  sudo update-desktop-database
+fi
